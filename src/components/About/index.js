7c040142ba8bab80f4d33fe9cd7a36f256962bc4
andytang02 <andytang02@gmail.com>
2020-12-27 22:42:11 -0800

    about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,7 +3,17 @@ import React from 'react';
 function About() {
 
   return (
-    <h1>About</h1>
+    <section className="my-5">
+      <h1>About Me</h1>
+      <div className = "flex-row space-around">
+        <img src={require("../../assets/pfp.jpeg").default} className="my-2" style={{ width: "33%" }} alt="Profile Picture" />
+        <h3>
+            I'm currently a first year student at UCLA pursuing a double major in Math and Computer Science. 
+            I have experience in full-stack web development, machine learning, and algorithmic problem solving. 
+            I love fitness, music, and comedy.
+        </h3>
+      </div>
+    </section>
   );
 }
 
