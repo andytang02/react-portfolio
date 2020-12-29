d79e38bf575fceb0d658c11a3351101298b427ba
andytang02 <andytang02@gmail.com>
2020-12-29 12:02:05 -0800

    resume section

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -3,7 +3,12 @@ import React from 'react';
 function Resume() {
 
   return (
-    <h1>Resume</h1>
+    <section className="my-5">
+      <h1>Resume</h1>
+      <h3>Download my resume <a href = {require("../../assets/Resume.pdf").default} download="Resume.pdf"> here </a></h3>
+      <h5>Languages: Java, C/C++, Python, Javascript, HTML/CSS, R</h5>
+      <h5>Proficiencies: MongoDB, Express.js, React.js, Node.js, MySQL</h5>
+    </section>
   );
 }
 
